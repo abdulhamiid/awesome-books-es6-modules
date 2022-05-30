bd6ae4a295fc146a39ef7d45e3595054a80565bd
abdulhamiid <adioabdulhamid@yahoo.com>
2022-05-30 15:08:05 +0100

    Import js files from the modules directory

---
+++ index.js
@@ -0,0 +1,8 @@
+import { createBook } from "./modules/instantiate.js";
+import { pageSwitcher } from "./modules/switcher.js";
+import { currentDate } from "./modules/date.js";
+
+
+createBook();
+currentDate();
+pageSwitcher();
