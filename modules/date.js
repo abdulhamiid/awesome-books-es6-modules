03e2e4624b7a863923a998bfde9c5f8563c7d12e
abdulhamiid <adioabdulhamid@yahoo.com>
2022-05-30 15:52:36 +0100

    Update date function using lexon

--- modules/date.js
+++ modules/date.js
@@ -1,5 +1,6 @@
+import { DateTime } from "./luxon.js";
 const datePlaceholder = document.querySelector('#date');
-
 export const currentDate = () => {
-  datePlaceholder.textContent = new Date();
+  let dt = DateTime.now();
+  datePlaceholder.textContent = dt.toLocaleString(DateTime.DATETIME_MED);
 }
