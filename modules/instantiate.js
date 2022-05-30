8e29b5fb27218219916aeee6d1fb5b9f3c3885c7
abdulhamiid <adioabdulhamid@yahoo.com>
2022-05-30 15:05:14 +0100

    Add a page-switcher function and instantiate.js file

---
+++ modules/instantiate.js
@@ -0,0 +1,14 @@
+import { BookCollection } from "./saveBooksData.js";
+
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const booksStore = [];
+
+const Book1 = new BookCollection(bookTitle, bookAuthor, booksStore);
+
+// instatiate
+export const createBook = () => {
+  Book1.saveData();
+  Book1.removeData();
+  Book1.existingData();
+}
---
+++ modules/switcher.js
@@ -0,0 +1,15 @@
+const switcher = document.querySelectorAll('[data-switcher]');
+
+export const pageSwitcher = () => {
+  for (let i = 0; i < switcher.length; i += 1) {
+    const dataSwitch = switcher[i];
+    const pageId = dataSwitch.dataset.tab;
+    dataSwitch.addEventListener('click', () => {
+      document.querySelector('.is-Active').classList.remove('is-Active');
+      dataSwitch.classList.add('is-Active');
+      document.querySelectorAll('.page').forEach((page) => { page.classList.remove('is-Active'); });
+      const nextPage = document.querySelector(`.home-page .page[data-page = "${pageId}"]`);
+      nextPage.classList.add('is-Active');
+    });
+  }
+}
